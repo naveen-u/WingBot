e99ce535e76cb0ac03a1345768e2cd951a1773d1
Naveen Unnikrishnan <unnaveen@amazon.com>
2020-04-19 19:06:44 +0530

    Adding signal handler for exiting elegantly

--- wingbot.py
+++ wingbot.py
@@ -4,11 +4,26 @@ from discord.ext import commands
 from dotenv import load_dotenv
 from utils.configManager import BotConfig
 from utils.help import HelpCommand
+import signal
+import sys
+import asyncio
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 config = BotConfig()
 bot = commands.Bot(command_prefix = config.commandPrefix, case_insensitive = True)
+
+async def signalHandler():
+    """
+    Signal handler to perform required cleanup operations before quitting bot
+    """
+    print('Cleaning up the mess...')
+    for item in config.cogs:
+        cog = bot.get_cog(item.capitalize())
+        print(f'Executing signal handlers of the {item} cog...')
+        if hasattr(cog, 'signalHandler'):
+            cog.signalHandler()
+    await bot.close()
 
 @bot.event
 async def on_ready():
@@ -24,8 +39,12 @@ async def on_ready():
             bot.load_extension(cogDirectory + '.' + cog)
         except commands.errors.ExtensionAlreadyLoaded:
             pass
+    loop = asyncio.get_event_loop()
+    for signame in ('SIGINT', 'SIGTERM'):
+        loop.add_signal_handler(getattr(signal, signame), lambda: asyncio.ensure_future(signalHandler()))
     return
 
+# Adding custom help command
 helpCommand = HelpCommand()
 bot.help_command = helpCommand
 bot.run(TOKEN)
